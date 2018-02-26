b2e36aeb6d1e4931d2a3d84384f286067d01a79a
Chase Bowden and Hannah Lee <chase@>
2018-02-26 11:27:26 -0800

    add list.erb, html forms and front end get and post requests for list.

--- app.rb
+++ app.rb
@@ -3,17 +3,36 @@ require('sinatra/reloader')
 require('sinatra/activerecord')
 also_reload("lib/**/*.rb")
 require('./lib/task')
+require('./lib/list')
 require('rspec')
 require('pg')
 require('pry')
 
 get('/') do
   @tasks = Task.all()
+  @lists = List.all()
   erb(:index)
 end
 
-# post('lists') do
-# end
+post('/') do
+  name = params[:name]
+  new_list = List.create({:name => name})
+  @lists = List.all()
+  erb(:index)
+end
+
+get('/lists/:id')do
+  @list = List.find_by({:id => params[:id]})
+  erb(:list)
+end
+
+post('/lists/:id') do
+  description = params[:description]
+  @list = List.find_by(id: params[:id])
+  @list.tasks().create({:description => description})
+  binding.pry
+
+end
 
 get('/tasks/:id/edit') do
   @task = Task.find(params.fetch("id").to_i())
--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -9,4 +9,24 @@ describe(List) do
      expect(test_list.tasks()).to(eq([test_task1, test_task2]))
     end
   end
+  describe("#create") do
+    it("adds a new list to the database") do
+      test_list = List.create({:name => "list"})
+      expect(List.all).to(eq([test_list]))
+    end
+  end
+  describe('#update') do
+    it("updates info about list in database") do
+      test_list = List.create(name: "list")
+      test_list.update(:name => "new list")
+      expect(test_list.name).to(eq("new list"))
+    end
+  end
+  describe('#find')do
+    it("finds a list by a particular parameter") do
+      test_list = List.create(name: "list")
+      # found_list = List.find_by(name: "list")
+      expect(List.find_by(name: "list")).to(eq(test_list))
+    end
+  end
 end
